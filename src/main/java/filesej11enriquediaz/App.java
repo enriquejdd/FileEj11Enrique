87beb5d9a0a3667a21f2e861f728b0a020a8eac9
enriquejdd <enriquejdd1999@gmail.com>
2021-04-26 19:00:11 +0200

    Añadimos métodos descripcionAleatorio(), fechasAleatorias() y crearAppAleatoria().  faltando en este último solo el DoubleStream

--- src/main/java/filesej11enriquediaz/App.java
+++ src/main/java/filesej11enriquediaz/App.java
@@ -88,4 +88,66 @@ public class App {
         return aleatorio + "\t" + codigo + "\t" + nombre + "\t" + descripcion + "\t" + tamanioKB + "\t" + fecCreacion;
     }
 
+    public void crearAppAleatoria() {
+        App appAleatoria = new App();
+        int contador = 0;
+        String abecedario = "abcdefghijklmnopqrstuvwxyz";
+        int numLetraAbec = aleatorio.nextInt(abecedario.length());
+        String nomb = "app" + contador + abecedario.charAt(numLetraAbec);
+        String descripcionApp = descripcionAleatorio();
+        int tamanio1 = aleatorio.nextInt(1024) + 100;
+        Double tamanio2 = aleatorio.nextDouble();
+        
+        LocalDate fecAleatoria = fechasAleatorias();
+
+    }
+
+    public String descripcionAleatorio() {
+        ArrayList<String> descripciones = new ArrayList<>();
+        descripciones.add("Aplicacion de Desarrollo");
+        descripciones.add("Aplicacion de Cuentas");
+        descripciones.add("Aplicacion de Viajes");
+        descripciones.add("Aplicacion de Compras");
+        descripciones.add("Aplicacion de Videos");
+        descripciones.add("Aplicacion de Mensajeria");
+        descripciones.add("Aplicacion de Red_Social");
+        descripciones.add("Aplicacion de Juegos");
+        descripciones.add("Aplicacion de Llamadas");
+        descripciones.add("Aplicacion de Creación");
+
+        String descripcionApp = descripciones.get(aleatorio.nextInt(descripciones.size()));
+        return descripcionApp;
+    }
+
+    public LocalDate fechasAleatorias() {
+        int año = aleatorio.nextInt(2022 + 1995 - 1995);
+        int mes = aleatorio.nextInt(12) + 1;
+        int dia = 0;
+        switch (mes) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                dia = aleatorio.nextInt(31) + 1;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                dia = aleatorio.nextInt(30) + 1;
+                break;
+            case 2:
+                if (año % 4 == 0) {
+                    dia = aleatorio.nextInt(29) + 1;
+                } else {
+                    dia = aleatorio.nextInt(28) + 1;
+                }
+                break;
+        }
+
+        return LocalDate.of(año, mes, dia);
+    }
 }
