8a00a0567ee4641560894958b0c08f28f7dd76af
enriquejdd <enriquejdd1999@gmail.com>
2021-04-26 18:59:05 +0200

    Añadimos getters,setters y toString ya formateado

--- src/main/java/filesej11enriquediaz/App.java
+++ src/main/java/filesej11enriquediaz/App.java
@@ -6,6 +6,8 @@
 package filesej11enriquediaz;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -13,11 +15,14 @@ import java.util.Random;
  * @author Enrique
  */
 public class App {
-    int codigo;
-    String nombre;
-    String descripcion;
-    Double tamanioKB;
-    LocalDate fecCreacion;
+
+    private Random aleatorio = new Random();
+
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+    private Double tamanioKB;
+    private LocalDate fecCreacion;
 
     public App() {
     }
@@ -28,6 +33,59 @@ public class App {
         this.descripcion = descripcion;
         this.tamanioKB = tamanioKB;
         this.fecCreacion = fecCreacion;
+    }
+
+    public LocalDate getFecCreacion() {
+        return fecCreacion;
+    }
+
+    public void setFecCreacion(LocalDate fecCreacion) {
+        this.fecCreacion = fecCreacion;
+    }
+
+    public Random getAleatorio() {
+        return aleatorio;
+    }
+
+    public void setAleatorio(Random aleatorio) {
+        this.aleatorio = aleatorio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getTamanioKB() {
+        return tamanioKB;
+    }
+
+    public void setTamanioKB(Double tamanioKB) {
+        this.tamanioKB = tamanioKB;
+    }
+
+    @Override
+    public String toString() {
+        return aleatorio + "\t" + codigo + "\t" + nombre + "\t" + descripcion + "\t" + tamanioKB + "\t" + fecCreacion;
     }
 
 }
